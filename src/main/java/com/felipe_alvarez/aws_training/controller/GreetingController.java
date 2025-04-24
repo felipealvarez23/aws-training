251948079df84bb966d5914c4a978deb6680ce12
Edwin Felipe Alvarez Rincon <efalvare@bancolombia.com>
2025-04-23 19:13:14 -0500

    Base project

---
+++ src/main/java/com/felipe_alvarez/aws_training/controller/GreetingController.java
@@ -0,0 +1,14 @@
+package com.felipe_alvarez.aws_training.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("greeting")
+    public String sayHello() {
+        return "Hello my friend!";
+    }
+
+}
---
+++ src/test/java/com/felipe_alvarez/aws_training/AwsTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe_alvarez.aws_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
